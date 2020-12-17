3bb31295eb837ea0781b73f419983acc7a78faf5
EmilienBidet <emilienbdt@gmail.com>
2020-12-17 23:48:13 +0100

    Add second method to build leafs of the chrismas tree with more algorithm concepts and modulable

--- chrismas_tree.py
+++ chrismas_tree.py
@@ -1,3 +1,17 @@
+def make_branchs(floors_number=3, branch_number_per_floor=4):
+    tree = ""
+    leafs_on_top = 1
+    modificateur_leaf = 2
+    for floor in range(floors_number):
+        leafs = leafs_on_top
+        for branch in range(branch_number_per_floor):
+            tree += SPACE * (OFF_SET * (floors_number - floor)) + SPACE * ((branch_number_per_floor - branch-1)*int(modificateur_leaf/2)) + LEAF * leafs + "\n"
+            leafs += modificateur_leaf
+        leafs_on_top += 2
+        modificateur_leaf += 2
+    return tree.rsplit('\n', 1)[0]
+
+
 def make_branchs_first():
     FLOORS = 4
     stars = 1
